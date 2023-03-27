691f62c80f6f10a840350cb0f4098035c51f3b19
Wei Jin <swissjw@gmail.com.com>
2023-03-27 12:01:32 -0400

    Updated

--- ProgrammingConceptes/src/AdvancedJavaProgramming/RMI.java
+++
@@ -1,9 +0,0 @@
-package AdvancedJavaProgramming;
-
-/**
- * RMI : Remote Method Invocation : provides a framework for building distributed Java systems;
- *       enables you to access a remote object and invoke its methods.
- *       a high-level API built on top of sockets
- */
-public class RMI {
-}
